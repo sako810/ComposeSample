9e7e83bd3a0fc78a20b007dcb70c6f2961031064
hiraok <hiraok.work@gmail.com>
2023-11-08 16:11:17 +0900

    diff8

--- app/src/main/java/net/hiraok/composesample/MainActivity.kt
+++ app/src/main/java/net/hiraok/composesample/MainActivity.kt
@@ -97,7 +97,7 @@ fun Home() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("init diff diff7")
+        Text("init diff diff8")
     }
 }
 
