d41ce178f8488383869cff8ee883ffece9729bff
sako810 <hiraok.work@gmail.com>
2024-08-11 21:56:03 +0900

    fix

--- app/src/test/java/net/hiraok/compose_sample/TopScreenRobot.kt
+++ app/src/test/java/net/hiraok/compose_sample/TopScreenRobot.kt
@@ -1,16 +1,19 @@
 package net.hiraok.compose_sample
 
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.ui.test.isRoot
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.takahirom.roborazzi.DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.RoborazziRule
 import com.github.takahirom.roborazzi.captureRoboImage
+import net.hiraok.feature.top.SecondScreen
+import net.hiraok.feature.top.TopScreen
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.GraphicsMode
+import java.io.File
 
 @ExperimentalMaterial3Api
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
@@ -18,13 +21,32 @@ import org.robolectric.annotation.GraphicsMode
 class TopScreenRobot {
 
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<MainActivity>()
+    val roborazziRule = RoborazziRule(
+        options = RoborazziRule.Options(
+            outputDirectoryPath = DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH,
+            outputFileProvider = { description, outputDirectory, fileExtension ->
+                File(
+                    outputDirectory,
+                    "${description.methodName}.$fileExtension"
+                )
+            }
+        ),
+    )
 
     @Config(qualifiers = RobolectricDeviceQualifiers.Pixel7)
     @Test
-    fun `スクリーンテストで表示される`() {
-        composeTestRule.onNode(isRoot())
-            .captureRoboImage("build/outputs/roborazzi/screenshotTest.png")
+    fun `TopScreenCapture`() {
+        captureRoboImage {
+            TopScreen()
+        }
+    }
+
+    @Config(qualifiers = RobolectricDeviceQualifiers.Pixel7)
+    @Test
+    fun `SecondScreenCapture`() {
+        captureRoboImage {
+            SecondScreen()
+        }
     }
 
 }
