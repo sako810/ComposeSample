68393493392acb78aad53df87f77d84268eda890
sako810 <hiraok.work@gmail.com>
2024-08-11 18:59:25 +0900

    test1追加

--- feature/top/src/main/java/net/hiraok/feature/top/TopScreen.kt
+++ feature/top/src/main/java/net/hiraok/feature/top/TopScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 @Composable
 fun TopScreen() {
     Column {
-        Text(text = "test")
+        Text(text = "test0")
+        Text(text = "test1")
     }
 }
